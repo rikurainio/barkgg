50f6f338f3163308de5707cc3f45088d52bc8b6c
Riku Rainio <riku.rainio@tuni.fi>
2022-02-05 14:14:49 +0200

    pages checkpoint

--- pages/livegame/index.js
+++ pages/livegame/index.js
@@ -1,9 +1,20 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
 export default function LiveGame(){
+
+    const { colorMode, toggleColorMode } = useColorMode()
+
     return (
         <Box>
-            livegame lol
+            <Flex background={colorMode === 'light' ? "#F8F8F8" : "black"}
+                    backgroundImage={colorMode === 'light' ? 'lux.jpg' : 'backgrounds/xinzhaoart.png'}
+                    backgroundSize={"100%"}
+                    backgroundRepeat={"no-repeat"}
+                    height={"2100px"}
+                    as="div" 
+                    className="content-container"
+                    justifyContent={"center"}>
+            </Flex>
         </Box>
     )
 }
