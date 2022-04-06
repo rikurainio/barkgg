ec1ef438192182262c9a93aa0c7808c26aba7550
Riku Rainio <rkurainio@gmail.com>
2022-04-06 10:51:53 +0300

    fix and update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
-import { Flex, Text, Box, HStack, useColorModeValue } from '@chakra-ui/react'
+import { Flex, Text, Box, useColorModeValue, Link } from '@chakra-ui/react'
+import Twitter from 'react-color/lib/components/twitter/Twitter';
+import { FaTwitter, FaDiscord } from 'react-icons/fa';
+
 
 const Footer = () => {
     const modeColorsFooterNavbar = useColorModeValue('rgb(245, 245, 250)', 'rgb(53, 54, 51)')
 
     return (
         <Flex
+            height={'360px'}
             justifyContent={"center"}
             className="footer--pin"
             backgroundColor={modeColorsFooterNavbar}
@@ -16,23 +20,31 @@ const Footer = () => {
                 as="h1"
             >
                 <Box>
-                    <HStack paddingTop={"6px"} spacing={"30px"}>
-                        <Text>
-                            About
-                        </Text>
-                        <Text>
-                            Copyright Notice
-                        </Text>
-                        <Text>
-                            Sitemap
-                        </Text>
-                        <Text>
-                            Contact
-                        </Text>
-                        <Text>
+                    <Flex flexDir={'column'} paddingTop={"20px"} spacing={"30px"}>
+                        <Text fontSize={'50px'} textAlign={'center'}>
                             Socials
                         </Text>
-                    </HStack>
+                        <Flex justifyContent={'center'} alignContent={'center'} textAlign={'center'}>
+                            <Flex flexDir={'column'} marginRight={'10px'}>
+                                <Link href='https://twitter.com/RhanLoL'>
+                                    <FaTwitter fontSize={'80px'}></FaTwitter>
+                                </Link>
+                                <Text>
+                                    @RhanLoL
+                                </Text>
+                            </Flex>
+
+                            <Flex flexDir={'column'} justifyContent={'center'} alignContent={'center'} marginLeft={'10px'}>
+                                <Link href='https://discord.com/users/107596792421449728'>
+                                    <FaDiscord fontSize={'80px'}></FaDiscord>
+                                </Link>
+                                <Text>
+                                    Rhan#5498
+                                </Text>
+                            </Flex>
+
+                        </Flex>
+                    </Flex>
                 </Box>
             </Box>
         </Flex>
